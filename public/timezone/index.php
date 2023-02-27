4f337122e922c34c7e5f8be28db464f4428a2010
ubed <ijul@gmail.com>
2023-02-27 10:26:07 +0700

    init

---
+++ public/timezone/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// diable cors
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+$timeZoneConnect = mysqli_connect("localhost", "root", "", "country_time_zone");
+$response = new StdClass();
+
+if(!$timeZoneConnect) {
+    $response->message = "database error";
+    $response->data = [];
+    print(json_encode($response));
+    exit;
+}
+
+$response->message = "ok";
+$key = $_GET['key'];
+$query = mysqli_query($timeZoneConnect, "SELECT * FROM time_zone WHERE country_name LIKE '%$key%' LIMIT 10");
+$timeZones = [];
+while($data = mysqli_fetch_assoc($query)) {
+    $timeZones[] = $data;
+}
+$response->data = $timeZones;
+$response = json_encode($response);
+
+print($response);
+
+?>
